51bb8c7dc6c114f01f2ca3b0d0dc4e896ec1826c
An Huynh <An Huynh>
2016-09-16 13:30:28 -0500

    finished hw, no bonus yet...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 //showing current date and time on the webpage -- setting up for calculating time
-	var currentTimeView = moment().format('LTS');
+	var currentTimeView = moment().format('LT');
 	var currentDateView = moment().format('LL');
 
 	$('#currentTime').append(currentTimeView);
 	$('#currentDate').append(currentDateView);
 
 
-// Initialize Firebase
+// Initialize firebase
 var config = {
 	apiKey: "AIzaSyBjjNPRDXKAntvJnoE1d7n9qAeTOEc8sbA",
 	authDomain: "train-ing.firebaseapp.com",
@@ -59,36 +59,37 @@ $("#submitTrain").on("click", function() {
 database.ref().on("child_added", function(childSnapshot, prevChildKey){
 	var newTrainName = childSnapshot.val().name;
 	var newDestination = childSnapshot.val().destination;
+
 	var newFrequency = childSnapshot.val().frequency;
 	var newTime = childSnapshot.val().beginTime;
 
 	console.log(childSnapshot.val());
 
+	//Calculating time 
+	//var trainFrequency = 15;
+	//var firstTrainTime = "4:30";
+	var firstTrainTimeConverted = moment(newTime, 'hh:mm').subtract(1, 'years');
+	var diffTime = moment().diff(moment(firstTrainTimeConverted), 'minutes');
+	var timeRemainder = diffTime % newFrequency;
+	var timeMinutesUntilTrain = newFrequency - timeRemainder;
+	var nextTrain = moment().add(timeMinutesUntilTrain, "hh:mm");
+
+	console.log('time: ' + newTime);
+	console.log('freq: ' + newFrequency);
+
+	console.log("minute values: " + firstTrainTimeConverted);
+	console.log("current Time: " + currentTimeView);
+	console.log('difference in minutes: ' + diffTime);
+	console.log("time remainder: " + timeRemainder);
+	console.log("minutes til train: " + timeMinutesUntilTrain);
+	console.log("arrival time: " + moment(nextTrain).format('LT'));
 
 	
-	$('#trainScheduale	> tbody').append('<tr><td>' + newTrainName + '</td><td>' + newDestination + '</td><td>' + newFrequency + '</td><td>' + timeCalculation.nextTrain + '</td><td>' + timeCalculation.timeMinutesUntilTrain + '</td></tr>');
+	$('#trainScheduale	> tbody').append('<tr><td>' + newTrainName + '</td><td>' + newDestination + '</td><td>' + newFrequency + '</td><td>' + moment(nextTrain).format('LT') + '</td><td>' + timeMinutesUntilTrain + '</td></tr>');
 });
 
-function timeCalculation() {
 
-	//Calculating time left until next train stop
-	var trainFrequency = 15;
-	var firstTrainTime = "4:30";
-	var firstTrainTimeConverted = moment(firstTrainTime, 'hh:mm').subtract(1, 'years');
-	console.log(firstTrainTimeConverted);
-	console.log("current Time: " + currentTimeView);
 
-	var diffTime = moment().diff(moment(firstTrainTimeConverted), 'minutes');
-	console.log('difference in minutes: ' + diffTime);
 
-	var timeRemainder = diffTime % trainFrequency;
-	console.log("time remainder: " + timeRemainder);
-
-	var timeMinutesUntilTrain = trainFrequency - timeRemainder;
-	console.log("minutes til train: " + timeMinutesUntilTrain);
-
-	var nextTrain = moment().add(timeMinutesUntilTrain, "minutes");
-	console.log("arrival time: " + moment(nextTrain).format('LTS'));
-}
 
 
